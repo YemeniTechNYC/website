7506240445827a8547a95cbe7e8db36da675a109
salah ahmed <salahs.email@pm.me>
2023-11-19 18:07:20 -0500

    Add posts app (#6)

--- our_blog_api/settings.py
+++ our_blog_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # our apps
+    "posts.apps.PostsConfig",
     "user.apps.UserConfig",
 ]
 AUTH_USER_MODEL = "user.User"
