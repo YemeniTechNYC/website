df13084a7e217ce80807f4dea95868ae9d32a3d8
salah ahmed <salahs.email@pm.me>
2023-11-19 18:06:10 -0500

    Add User Model (#5)
    * add user model

--- our_blog_api/settings.py
+++ our_blog_api/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # our apps
+    "user.apps.UserConfig",
 ]
+AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
