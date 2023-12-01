39f99ce964c594f681ec2d5060e6cd8876c8a530
salah ahmed <salahs.email@pm.me>
2023-12-01 18:27:22 -0500

    rename to yit (yemenis-in-tech)

--- yit/asgi.py
+++ yit/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for our_blog_api project.
+ASGI config for yit project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "our_blog_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yit.settings")
 
 application = get_asgi_application()
--- yit/wsgi.py
+++ yit/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for our_blog_api project.
+WSGI config for yit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "our_blog_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yit.settings")
 
 application = get_wsgi_application()
