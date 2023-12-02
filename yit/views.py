8cab33cda10866727098275925effbee2deff8dd
salah ahmed <salahs.email@pm.me>
2023-12-02 14:54:20 -0500

    add base html

--- yit/views.py
+++ yit/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def health(request):
     return HttpResponse("OK", status=200)
+
+
+def index(request):
+    return render(request, "yit/index.html")
